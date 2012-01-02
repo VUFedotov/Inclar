af47b41430af34aec3e6572bf2f301d1fb14cf54
VUFedotov <vufedotov@gmail.com>
2012-01-02 13:06:44 +0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Inclar::Application.config.secret_token = 'd1519d75f75bf114b6f82b566134496edf37e51eb263a481630be11790380f518557895dab059911bd1a1a27ea17533fbe98aa3a3abfbbfc3bcb335e63405ef3'
